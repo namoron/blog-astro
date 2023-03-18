c1224d213ea28704919757b41bb8dbd1cdc01c53
namor <n2210478@gl.cc.uec.ac.jp>
2023-03-18 14:24:47 +0900

    ヘッダーが一瞬消えるバグの修正

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,9 @@
 document.querySelector(".hamburger").addEventListener("click", () => {
   document.querySelector(".nav-links").classList.toggle("expanded");
 });
+
+// ヘッダーが消えるのを修正する
+window.addEventListener("load", function () {
+  var header = document.querySelector(".header");
+  header.style.display = "block";
+});

17e762d64f17b84f04edae2493e2ad118b925e07
namor <n2210478@gl.cc.uec.ac.jp>
2023-03-22 11:23:45 +0900

    6-3にもどす

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,11 +1,3 @@
 document.querySelector(".hamburger").addEventListener("click", () => {
   document.querySelector(".nav-links").classList.toggle("expanded");
 });
-
-// ヘッダーが消えるのを修正する
-// export default {
-//   onMount() {
-//     const header = document.querySelector(".header");
-//     header.style.display = "block";
-//   },
-// };

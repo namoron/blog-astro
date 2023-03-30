54e152643f16dc2958965a04d5310984a829a1b7
namor <n2210478@gl.cc.uec.ac.jp>
2023-03-30 11:57:58 +0900

    レス変更　ととのえ

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -10,7 +10,7 @@ export default function Greeting({ messages }) {
   return (
     <div>
       <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
+      {/* <button onClick={() => setGreeting(randomMessage())}>New Greeting</button> */}
     </div>
   );
 }
